870f92f0c48ae694bb51cbc7b8d52acf9da37589
lattie <none>
2021-06-25 09:23:12 +0800

    test

--- CentOS_Base_Configure.sh
+++ CentOS_Base_Configure.sh
@@ -1,42 +1,66 @@
 # Name: CentOS base configure on click execute
 # Author: Lattie
 # Build Time: 2021-06-21 20: 56
-# Last Modify Time: 2021-06-21 20:57
+# Last Modify Time: 2021-06-25 09:22
 #!/bin/bash
 
 # modify yum repo
-echo -e "Modify yum repo.\n"
 
-mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
-
-echo ""
-
-echo -ne "Choose CentOS version："
-
-read version_number
-
-echo " \
-1. CentOS 5 \
-2. CentOS 6 \
-3. CentOS 7"
-
-if [ $version_number == "2" ]
-then
-    wget http://mirrors.163.com/.help/CentOS6-Base-163.repo -O /etc/yum.repos.d/CentOS-Base.repo
-    yum clean all
-    yum makecache
-    yum repolist
-else [ $version_number == "3" ]
-then
-    wget http://mirrors.163.com/.help/CentOS7-Base-163.repo -O /etc/yum.repos.d/CentOS-Base.repo
-    yum clean all
-    yum makecache
-    yum repolist
-else [ $version_number == "1" ]
-then
-    wget http://mirrors.163.com/.help/CentOS5-Base-163.repo -O /etc/yum.repos.d/CentOS-Base.repo
-    yum clean all
-    yum makecache
-    yum repolist
-# install soft
+# variable
+yum_site_list=("163" "aliyun" "tsinghua")
 yum_soft_base=("iptables-services" "vim" "wget")
+yum_repo_aliyun=("http://mirrors.aliyun.com/repo/Centos-5.repo" "http://mirrors.aliyun.com/repo/Centos-6.repo" "http://mirrors.aliyun.com/repo/Centos-7.repo")
+yum_repo_163=("http://mirrors.163.com/.help/CentOS5-Base-163.repo" "http://mirrors.163.com/.help/CentOS6-Base-163.repo" "http://mirrors.163.com/.help/CentOS7-Base-163.repo")
+
+# function
+ChooseYumSite(){
+    if [ $1 == "aliyun" ]
+    then
+        ChooseSystemVersion
+        DownloadYumFile $choose
+    else [ $1 == "163" ]
+    then
+        ChooseSystemVersion
+        DownloadYumFile $choose
+    if [$1 == "tsinghua" ]
+    then
+        ChooseSystemVersion
+        DownloadYumFile $choose
+    fi 
+}
+
+ChooseSystemVersion(){
+    read -p "Choose system version: \
+    1. CentOS7 \
+    2. CentOS6 \
+    3. CentOS5" $choose
+}
+
+DownloadYumFile(){
+    if [ $1 == "1" ]
+    then
+        wget $2 -O /etc/yum.repos.d/CentOS-Base.repo
+        yum clean all
+        yum makecache
+        yum repolist
+    else [ $1 == "2" ]
+    then
+        wget $2 -O /etc/yum.repos.d/CentOS-Base.repo
+        yum clean all
+        yum makecache
+        yum repolist
+    else [ $1 == "3" ]
+    then
+        wget $2 -O /etc/yum.repos.d/CentOS-Base.repo
+        yum clean all
+        yum makecache
+        yum repolist
+    fi
+}
+
+read -p "Choose yum site: \
+1. Aliyun \
+2. 163 \
+3. Tsinghua" $choose
+
+ChooseYumSite $choose
